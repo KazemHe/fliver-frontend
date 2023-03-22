43554814f91a883bdf032873e99d2229bbc0e1b4
esther kaptsan <estherkaptsan@gmail.com>
2023-03-22 15:27:28 +0200

    header

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,10 +1,6 @@
 <template>
   <section class="container home text-center">
     <h1>fiver</h1>
-    <h5 class="text-center">
-      Powered up Frontend 
-    </h5>
-    <img alt="Vue logo" src="../assets/logo.png">
   </section>
 
   
