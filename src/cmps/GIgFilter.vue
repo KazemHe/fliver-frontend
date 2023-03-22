4bdb076212aeb85e22be4e5edcff946574c744cc
KazemHe <kazemhe1@gmail.com>
2023-03-22 23:23:53 +0200

    filetr

--- src/cmps/GIgFilter.vue
+++ src/cmps/GIgFilter.vue
@@ -1,15 +1,8 @@
 <template>
   <section class="filter">
-    <div class="menu-title more-filter-menu">Service Options<span class="nFghBOe chevron-icon-down"
-        style="width:10px;height:10px" aria-hidden="true">
-        <svg width="16" height="16" viewBox="0 0 11 7" xmlns="http://www.w3.org/2000/svg" fill="currentFill">
-          <path
-            d="M5.464 6.389.839 1.769a.38.38 0 0 1 0-.535l.619-.623a.373.373 0 0 1 .531 0l3.74 3.73L9.47.61a.373.373 0 0 1 .531 0l.619.623a.38.38 0 0 1 0 .535l-4.624 4.62a.373.373 0 0 1-.531 0Z">
 
-          </path>
-        </svg></span></div>
 
-    <div>
+    <!-- <div> -->
       <select @change="setFilterBy" v-model="sortBy.value">
         <button> service options </button>
         <option value="blog">blog </option>
@@ -17,7 +10,7 @@
         <option value="createdAt">prototype</option>
       </select>
 
-    </div>
+    <!-- </div> -->
     <select @change="setFilterBy" v-model="sortBy.value">
       <option value="blog">blog </option>
       <option value="price">e-commers</option>
@@ -29,16 +22,18 @@
       <option value="createdAt">prototype</option>
     </select>
   </section>
-
   <section class="sort-bar">
-    55 Services available
+     55 Services available
 
-  <select @change="setFilterBy" v-model="sortBy.value">
-      sort by: 
+
+     <div class="menu-title sort-filter">Best Selling<span class="nFghBOe chevron-icon-down" style="width:10px;height:10px" aria-hidden="true"><svg width="16" height="16" viewBox="0 0 11 7" xmlns="http://www.w3.org/2000/svg" fill="currentFill"><path d="M5.464 6.389.839 1.769a.38.38 0 0 1 0-.535l.619-.623a.373.373 0 0 1 .531 0l3.74 3.73L9.47.61a.373.373 0 0 1 .531 0l.619.623a.38.38 0 0 1 0 .535l-4.624 4.62a.373.373 0 0 1-.531 0Z"></path></svg></span></div>
+
+    <!-- <select @change="setFilterBy" v-model="sortBy.value">   
+      sort by:
       <option value="blog">blog </option>
       <option value="price">e-commers</option>
       <option value="createdAt">prototype</option>
-    </select>
+    </select>  -->
 
   </section>
 </template>
