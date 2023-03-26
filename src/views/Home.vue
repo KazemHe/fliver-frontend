f59e1b147c323537d91fbbdcdc2b205ec3199635
esther kaptsan <estherkaptsan@gmail.com>
2023-03-26 12:37:55 +0300

    home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,7 @@
   <section class=" hero-container home text-center full">
     <!-- <img src="https://fiverr-res.cloudinary.com/image/upload/q_auto,f_auto/v1/attachments/generic_asset/asset/bb5958e41c91bb37f4afe2a318b71599-1599344049983/bg-hero-1-1792-x1.png"/> -->
    
-      <div class="hero-search-container flex">
+      <div class="hero-search-container  flex">
         <h1>Find the perfect<span>freelance</span> services for your business</h1>
         <div class="hero-search flex">
           <div class="flex input-container">
